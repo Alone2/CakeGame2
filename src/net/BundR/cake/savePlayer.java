6c6f951ffb3d29ca728fb96cfb3f69347a06b163
Alone2 <a.sinzig02@gmail.com>
2017-11-22 19:56:45 +0100

    exp

--- src/net/BundR/cake/savePlayer.java
+++ src/net/BundR/cake/savePlayer.java
@@ -8,6 +8,7 @@ public class savePlayer {
 	public static void save(FileConfiguration cfg, String PlayerId, Player player) {
 		cfg.set("Player" + PlayerId + ".world", player.getLocation().getWorld().getName());
 		cfg.set("Player" + PlayerId + ".health", player.getHealth());
+		cfg.set("Player" + PlayerId + ".exp", player.getExp());
 		cfg.set("Player" + PlayerId + ".location.X", player.getLocation().getX());
 		cfg.set("Player" + PlayerId + ".location.Y", player.getLocation().getY());
 		cfg.set("Player" + PlayerId + ".location.Z", player.getLocation().getZ());
